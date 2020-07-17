d081ef1ec222e604bfda331e49744d6d813e1070
Calvin Lowe <calvinwolfelowe@gmail.com>
2020-07-17 15:14:39 -0700

    Fixed interval to not exponentially fire.

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import Stockcard from "./components/stockcard.jsx";
 
 function App() {
   const [body, setBody] = useState({name:"AMD", c:0, h:0, l:0, o:0, pc:0, t:0});
-  
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
+    console.log("Calling:")
+    console.log("in the getQuote")
     // let interval = null;
     let name = "AMD";
     //
     let token = "";
+
     function handleSetBody(body, name){
       setBody({...body, name:name});
+      setCount(count+1);
+      console.log(count)
     }
+    
     let interval = setInterval(() => {
       const request = require('request');
       request(`https://finnhub.io/api/v1/quote?symbol=${name}&token=${token}`, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-          handleSetBody(body, name)
-          // console.log(body)
+      if (err) { return console.log(err); }
+        handleSetBody(body, name) 
+      // console.log(`Sent: ${count}`);
       });
-    }, 30000);
-    console.log(interval)
-  })
-  
-  console.log(body)
+    }, 10000);
+    return ()=> clearInterval(interval)
+
+  });
 
   return (
     <div className="App">
